680b88a939c376064bae59feaf98a3a45165b67a
david hernandez <davidfelipehernandez1@gmail.com>
2022-07-03 20:59:42 -0500

    skills

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,14 +4,14 @@ import { AiOutlineArrowDown } from "react-icons/ai";
 
 function Welcome() {
     return (
-        <div className='welc'>
+        <section className='welc'>
             <div className='welc--info'>
                 <h1>David Hernandez</h1>
                 <h3>software developer</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, error doloremque. Nemo sint fugiat doloremque voluptatum possimus labore nostrum voluptate adipisci harum animi, nam veritatis accusantium fugit sit culpa dolore! Eum nihil nulla nisi adipisci eius doloribus expedita? Fugiat cupiditate est illo cumque quasi hic quae saepe molestiae sunt tempora.</p>
             </div>
             <button className='welc--btn'><AiOutlineArrowDown className='welc--icon' /></button>
-        </div>
+        </section>
     )
 }
 
--- src/components/body/Skills.jsx
+++ src/components/body/Skills.jsx
@@ -7,19 +7,21 @@ import python from '../../assets/python.svg'
 import java from '../../assets/java.svg'
 import git from '../../assets/git.svg'
 import github from '../../assets/github.svg'
+import '../../styles/skills.css'
 
 function Skills() {
     return (
-        <div>
-            <ul>
-                <li><img src={html} alt="html" width='100' /></li>
-                <li><img src={css} alt="css" width='100' /></li>
-                <li><img src={javascript} alt="html" width='100' /></li>
-                <li><img src={react} alt="html" width='100' /></li>
-                <li><img src={python} alt="html" width='100' /></li>
-                <li><img src={java} alt="html" width='100' /></li>
-                <li><img src={git} alt="html" width='100' /></li>
-                <li><img src={github} alt="html" width='100' /></li>
+        <div className='container--skills'>
+            <h2>Skills</h2>
+            <ul className='container--skills--list'>
+                <li className='container--skills--list--item'><img src={html} alt="html" width='100' />HTML5</li>
+                <li className='container--skills--list--item'><img src={css} alt="css" width='100' />CSS3 </li>
+                <li className='container--skills--list--item'><img src={javascript} alt="html" width='100' />JavaScript</li>
+                <li className='container--skills--list--item'><img src={react} alt="html" width='100' />React</li>
+                <li className='container--skills--list--item'><img src={python} alt="html" width='100' />Python</li>
+                <li className='container--skills--list--item'><img src={java} alt="html" width='100' />Java</li>
+                <li className='container--skills--list--item'><img src={git} alt="html" width='100' />Git</li>
+                <li className='container--skills--list--item'><img src={github} alt="html" width='100' />Github</li>
             </ul>
         </div>
     )
