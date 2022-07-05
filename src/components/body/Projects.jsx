338c1619e6dff8e0b4523e437cd1ae7ca92930b5
david hernandez <davidfelipehernandez1@gmail.com>
2022-07-04 21:16:51 -0500

    projects

---
+++ src/components/body/Projects.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../../styles/projects.css'
+
+function Projects() {
+    return (
+        <div className='projects'>
+            <h2>Projects</h2>
+            <ul>
+                <li>s</li>
+                <li>s2</li>
+                <li>s3</li>
+                <li>s4</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Projects
