c721d0e800bf65e711fd9b6cdf837005b8b2712c
david hernandez <davidfelipehernandez1@gmail.com>
2022-07-04 21:17:19 -0500

    style

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import Form from './components/Form'
 import Skills from './components/body/Skills'
+import Projects from './components/body/Projects'
 
 function App() {
 	return (
 		<div>
 			<NavBar />
 			<Skills />
+			<Projects />
 			<Form />
 			<Footer />
 		</div>
