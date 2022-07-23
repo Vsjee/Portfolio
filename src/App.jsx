400ca0864101ae1532e96fc6491fc9e27ea7ceda
david hernandez <davidfelipehernandez1@gmail.com>
2022-07-23 11:33:25 -0500

    global styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Form from './components/Form'
 import Skills from './components/body/Skills'
 import Projects from './components/body/Projects'
 import About from './components/body/About'
+import GlobalStyle from './globalStyles'
 
 function App() {
 	return (
 		<div>
+			<GlobalStyle />
 			<NavBar />
 			<About />
 			<Skills />
---
+++ src/globalStyles.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #ffffff;
+    margin: 0;
+    padding: 0;
+    p, a, ul, li {
+      font-size: 1.2rem;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      text-decoration: none;
+      list-style: none;
+    }
+    input, textarea {
+      font-size: 1.1rem;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+    h1, h2, h3, h4, h5, h6{
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+    }
+    button {
+      font-size: 1.1rem;
+    }
+  }
+`
+
+export default GlobalStyle
